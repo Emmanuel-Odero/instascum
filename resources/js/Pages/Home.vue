f26d973ef540f39edaecdd778eaf997240a5e8a7
Emmanuel-Odero <emmanuelodero808@gmail.com>
2023-07-26 18:57:06 +0000

    Project Initialised

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,15 @@
+<script setup>
+import MainLayout from '@/Layouts/MainLayout.vue';
+
+</script>
+
+<template>
+    <Head title="Home" />
+    <MainLayout>
+     </MainLayout>
+    
+</template>
+
+<style>
+
+</style>
