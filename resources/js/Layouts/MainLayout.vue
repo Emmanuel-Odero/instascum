1daae7f8292ca2f740ffeae48928ba856d0d526f
Emmanuel-Odero <emmanuelodero808@gmail.com>
2023-07-30 15:16:04 +0000

    vue components

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -10,7 +10,8 @@ import Plus from 'vue-material-design-icons/Plus.vue';
 import AccountOutline from 'vue-material-design-icons/AccountOutline.vue';
 import ChevronLeft from 'vue-material-design-icons/ChevronLeft.vue';
 import AccountPlusOutline from 'vue-material-design-icons/AccountPlusOutline.vue';
-let showCreatePost = ref (false);
+import MenuItem from '@/Components/MenuItem.vue';
+let showCreatePost = ref (false)
 </script>
 
 <template>
@@ -20,7 +21,40 @@ let showCreatePost = ref (false);
                 <Link href="/">
                     <img class="w-[105px] ml-6 cursor-pointer" src="/insta-logo.png" alt="Logo">
                 </Link>
+                <div class="flex items-center w-[50%]">
+                    <div class="flex items-center w-full bg-gra y-100 rounded-lg">
+                        <Magnify class="pl-4" fillColor="#8E8E8E" :size="27" />
+                        <input type="text" name="search" placeholder="Search " class="bg-tranparent w-full placeholder-[#8E8E8E] border-0 ring-0 focus:ring-0">
+                    </div>
+                    <HeartOutline class="pl-4 pr-3" fillColor="#000000" :size="27"/>
+                </div>
             </div>
+        </div>
+        <div v-if="$page.url !=='/'" class="md:hidden fixed flex items-center justify-between z-30 w-full bg-white h-[61px] border-b border-b-gray-300">
+            <Link href="/" class="px-4">
+                <ChevronLeft :size="30" class="cursor-pointer"/>
+            </Link>
+            <div class="font-extrabold text-lg">
+                Name Here
+            </div>
+            <AccountPlusOutline :size="30" class="cursor-pointer px-4"/>
+        </div>
+        <div id="SideNav" class="fixed h-full bg-white xl:w-[280px] w-80px md:block hidden border-r border-r-gray-300">
+            <Link href="/">
+                <img src="/insta-logo-small.png" alt="" class="xl:hidden block w-[25px] mb-10 cursor-pointer">
+                <img src="/insta-logo.png" alt="" class="xl:block hidden w-[120px] mt-10 ml-6 mb-10 cursor-pointer">
+            </Link>
+        </div>
+        <div class="px-3">
+            <Link href="/">
+                <MenuItem iconString="Home" class="mb-4 "/>
+            </Link>
+            <Link href="/">
+                <MenuItem iconString="Explore" class="mb-4 "/>
+            </Link>
+            <Link>
+                <MenuItem iconString="Plus" />
+            </Link>
         </div>
      </div>
      <!-- <slot/> -->
